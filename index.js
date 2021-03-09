0c08cf185734e0ca430ee844c2ead5e66a32c03a
Kimberly Gonzalez <ksgonzalez95@gmail.com>
2021-03-09 23:23:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function findMinAndRemove(array){
-
+  describe('#findMinAndRemove', function() {
+    it("should return the smallest element of an array", function() {
+      let array = [2, 3, 4, 5, 6, 7]
+      expect(findMinAndRemove(array)).toEqual(2)
+    });
 }
 
 function selectionSort(array){
